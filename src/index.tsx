e6555288f100a7014d6929ba7256c8fa0e2d97b4
mkato-manaby <mkato.manaby@gmail.com>
2024-02-01 14:42:49 +0900

    すべて終了

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+// index.js
 
-import { App } from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const root = createRoot(document.getElementById('app'));
-
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <StrictMode>
-    <App name="StackBlitz" />
-  </StrictMode>
+  <App />
 );
